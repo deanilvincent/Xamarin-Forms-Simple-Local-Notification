de6417a439184c099a12792aadba5f9970073f7d
Mark Deanil Vicente <markdeanilvicente@gmail.com>
2017-09-30 14:01:44 +0800

    Initial create

---
+++ MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SimpleLocalNotif
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            CrossLocalNotifications.Current.Show("Hey","You've triggered the notification");
+        }
+    }
+}
